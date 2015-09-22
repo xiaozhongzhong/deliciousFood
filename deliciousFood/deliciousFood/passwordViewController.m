401a800f632b8177b52732d3ea35e9a560c4a86c
admin1 <751232050@qq.com>
2015-09-22 16:42:56 +0800

    no message

--- deliciousFood/deliciousFood/passwordViewController.m
+++ deliciousFood/deliciousFood/passwordViewController.m
@@ -26,6 +26,15 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (BOOL)textFieldShouldReturn:(UITextField *)theTextField {
+    [theTextField resignFirstResponder];
+    return YES;
+}
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    [super touchesBegan:touches withEvent:event];
+    [self.view endEditing:YES];
+}
+
 /*
 #pragma mark - Navigation
 
@@ -36,11 +45,55 @@
 }
 */
 
-- (IBAction)canceAction:(UIBarButtonItem *)sender {
-}
+
 - (IBAction)cancelAction:(UIBarButtonItem *)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
 
 - (IBAction)doneAction:(UIBarButtonItem *)sender {
+    if (![self.oldpasswordTF.text isEqualToString:@""]&& ![self.newpasswordTF.text isEqualToString:@""]&&![self.newnewpassword.text isEqualToString:@""]) {
+        if ([self.oldpasswordTF.text isEqualToString:[Utilities getUserDefaults:@"passWord"]]) {
+            if ([self.newpasswordTF.text isEqualToString:self.newnewpassword.text]) {
+                
+                PFUser *currUser=[PFUser currentUser];
+                currUser.password=self.newpasswordTF.text;
+                UIActivityIndicatorView *aiv = [Utilities getCoverOnView:self.view];
+                [currUser saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+                    [aiv stopAnimating];
+                    if (succeeded) {
+                        [Utilities setUserDefaults:@"passWord" content:self.newpasswordTF.text];
+                        [Utilities popUpAlertViewWithMsg:@"成功修改！" andTitle:nil];
+                        [PFUser logOut];//退出Parse
+                        [aiv startAnimating];
+                        //重新登录
+                        [PFUser logInWithUsernameInBackground:currUser.username password:self.newpasswordTF.text block:^(PFUser *user, NSError *error) {
+                            
+                            [aiv stopAnimating];
+                            if (user) {
+                                [self dismissViewControllerAnimated:YES completion:nil];
+                            } else if (error.code == 101) {
+                                [Utilities popUpAlertViewWithMsg:@"用户名或密码错误" andTitle:nil];
+                            } else if (error.code == 100) {
+                                [Utilities popUpAlertViewWithMsg:@"网络不给力，请稍后再试" andTitle:nil];
+                            }else{
+                                [Utilities popUpAlertViewWithMsg:nil andTitle:nil];
+                            }
+                        }];
+                    } else {
+                        [Utilities popUpAlertViewWithMsg:nil andTitle:nil];
+                    }
+                }];
+            }else{
+                [Utilities popUpAlertViewWithMsg:@"俩次密码不一致，请重新输入" andTitle:nil];
+            }
+            
+        }else{
+            [Utilities popUpAlertViewWithMsg:@"与原密码不同，请重新输入" andTitle:nil];
+        }
+    }else{
+        [Utilities popUpAlertViewWithMsg:@"请填写所有内容，请重新输入" andTitle:nil];
+    }
+    
+
 }
 @end
