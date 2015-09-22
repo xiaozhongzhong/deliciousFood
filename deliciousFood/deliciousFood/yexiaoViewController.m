ba253913223b55c557ee75640d2e8ab5d655b172
gaoxiang <798185147@qq.com>
2015-09-22 11:41:51 +0800

    四个菜类的实现

--- deliciousFood/deliciousFood/yexiaoViewController.m
+++ deliciousFood/deliciousFood/yexiaoViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "yexiaoViewController.h"
-
+#import "caileiTableViewCell.h"
 @interface yexiaoViewController ()
 
 @end
@@ -17,13 +17,59 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self zaodian];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+-(void)zaodian{
+    PFQuery *query = [PFQuery queryWithClassName:@"Vegetables"];
+    [query whereKey:@"Type" equalTo:@"夜宵"];
+    UIActivityIndicatorView *aiv = [Utilities getCoverOnView:self.view];
+    [query findObjectsInBackgroundWithBlock:^(NSArray *returnedObjects, NSError *error) {
+        [aiv stopAnimating];
+        if (!error) {
+            _objectsForShow = returnedObjects;
+            [self.tableview reloadData];
+        } else {
+            NSLog(@"Error: %@ %@", error, [error userInfo]);
+        }
+    }];
+}
 
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return self.objectsForShow.count;
+}
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    caileiTableViewCell *cell=[tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
+    
+    PFObject *object=[self.objectsForShow objectAtIndex:indexPath.row
+                      ];
+    cell.foodname.text=object[@"Dishes"];
+    cell.price.text=[NSString stringWithFormat:@"%@元",object[@"Price"]];
+    cell.like.text=[NSString stringWithFormat:@"%@",object[@"Like"]];
+    cell.Umlike.text=[NSString stringWithFormat:@"%@",object[@"Unlike"]];
+    cell.jieshao.text=object[@"Discriptiondetail"];
+    
+    PFFile *photo =object[@"Photo"];
+    [photo getDataInBackgroundWithBlock:^(NSData *photoData, NSError *error) {
+        if (!error) {
+            UIImage *image = [UIImage imageWithData:photoData];
+            dispatch_async(dispatch_get_main_queue(), ^{
+                cell.imageVi.image = image;
+            });
+        }
+    }];
+    
+    return cell;
+    
+}
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    //回到当前页面,取消刚刚的选项
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
 /*
 #pragma mark - Navigation
 
