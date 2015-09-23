31e4c2acbf29fce37b5a911936a835e9a31fc38d
admin1 <751232050@qq.com>
2015-09-23 11:24:44 +0800

    购物车的完成和页面的实现

--- deliciousFood/deliciousFood/firstcellViewController.m
+++ deliciousFood/deliciousFood/firstcellViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "firstcellViewController.h"
-
+#import "secondViewController.h"
 @interface firstcellViewController ()
 - (IBAction)shopAction:(UIButton *)sender forEvent:(UIEvent *)event;
 - (IBAction)shoucangAction:(UIButton *)sender forEvent:(UIEvent *)event;
@@ -19,11 +19,17 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+//    NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc] init];
+//    paragraphStyle.alignment = NSTextAlignmentLeft;
+//    paragraphStyle.maximumLineHeight = 60;  //最大的行高
+//    paragraphStyle.lineSpacing = 5;  //行自定义行高度
+//    [paragraphStyle setFirstLineHeadIndent:self.label.frame.size.width + 5];//首行缩进 根据用户昵称宽度在加5个像素
+    
     // Do any additional setup after loading the view.
-
+    self.numbers.text=[NSString stringWithFormat:@"1"];
     self.label.text=self.item[@"Discriptiondetail"];
 
-    self.name.text=self.item[@"Dishes"];
+    self.name.text=[NSString stringWithFormat:@"菜名：  %@",self.item[@"Dishes"]];
     self.pirce.text=[NSString stringWithFormat:@"价格：  %@元",_item[@"Price"]];
     PFFile *photo =self.item[@"Photo"];
     [photo getDataInBackgroundWithBlock:^(NSData *photoData, NSError *error) {
@@ -54,7 +60,35 @@
 */
 
 - (IBAction)shopAction:(UIButton *)sender forEvent:(UIEvent *)event {
-    
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateFormat:@"yyyy-MM-dd HH:mm"];
+    NSString *strDate = [dateFormatter stringFromDate:[NSDate date]];
+    PFObject *shoping=[PFObject objectWithClassName:@"Shoppingcart"];
+    PFUser *currentUser = [PFUser currentUser];
+    shoping[@"ShopUser"]=currentUser;
+    shoping[@"timer"]=strDate;
+    shoping[@"Name"]=self.name.text;
+    NSNumberFormatter *numberFortnatters=[[NSNumberFormatter alloc]init];
+    [numberFortnatters setNumberStyle:NSNumberFormatterDecimalStyle];
+    NSNumber  *bentop= [numberFortnatters numberFromString:self.numbers.text];
+        shoping[@"TotalPrice"]=_item[@"Price"];
+    shoping[@"Bento"]=bentop;
+    PFFile *photo =self.item[@"Photo"];
+        shoping[@"Photo"] =photo;
+        UIActivityIndicatorView *aiv = [Utilities getCoverOnView:self.view];
+    [shoping saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+        [aiv stopAnimating];
+        if (succeeded) {
+            //回到上一页前更新页面的通知
+            // [[NSNotificationCenter defaultCenter] performSelectorOnMainThread:@selector(postNotification:) withObject:[NSNotification notificationWithName:@"refreshMine" object:self] waitUntilDone:YES];
+            //返回上一页
+            [Utilities popUpAlertViewWithMsg:@"加入购物车成功！" andTitle:nil];
+            [self.navigationController popViewControllerAnimated:YES];
+        } else {
+            [Utilities popUpAlertViewWithMsg:nil andTitle:nil];
+        }
+    }];
+
     
     
 }
@@ -92,5 +126,22 @@
 }
 
 - (IBAction)addAction:(UIStepper *)sender forEvent:(UIEvent *)event {
+    
+    
+    self.add.minimumValue=1;
+    self.add.maximumValue=5;
+    [self.add setStepValue:1.0];
+    [self.add setWraps:NO];
+    [self.add addTarget:self action:@selector(stepper:) forControlEvents:UIControlEventValueChanged];
+
+    self.numbers.text=[NSString stringWithFormat:@"%.0lf",self.add.value];
+    
+    
+    
 }
+///事件 控制值
+- (void)stepper:(UIStepper*)stepper{
+    self.numbers.text = [NSString stringWithFormat:@"%.0lf",self.add.value];
+}
+
 @end
